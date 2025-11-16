904a40818b80018103435c96c08ed05192f25857
MohammedAMizyed <mohammedamizayed2006@gmail.com>
2025-11-16 18:03:10 +0200

    First upload

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import { Button } from "./components/ui/button"
+import { cn } from "./lib/utils"
+import { CarouselProducts } from "./components/products-carousel"
+
+function App() {
+  const isTrue = false
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button
+        className={cn(
+          `cursor-pointer ${isTrue ? "bg-red-500" : "bg-amber-400"}`
+        )}
+      >
+        Click me
+      </Button>
+      <CarouselProducts />
+    </div>
+  )
+}
+
+export default App
