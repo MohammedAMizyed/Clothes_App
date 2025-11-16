eb5a44a67f692db21151ef06ed63e2b36eb4afe8
MohammedAMizyed <mohammedamizayed2006@gmail.com>
2025-11-16 18:17:41 +0200

    test committing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
 import { Button } from "./components/ui/button"
 import { cn } from "./lib/utils"
@@ -8,6 +5,7 @@ import { CarouselProducts } from "./components/products-carousel"
 
 function App() {
   const isTrue = false
+  const newVar = ""
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button
